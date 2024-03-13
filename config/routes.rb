a07175a9805d76a25fc88abf2fedbfd11033ff24
muunimoz <ec2-user@ip-172-31-26-69.ap-northeast-1.compute.internal>
2024-03-13 17:21:27 +0000

    [Finish]7

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+get 'top' => 'homes#top'
+resources :lists
 end
